98cc3d79bbab6da082ef0f99ae8ac0afa04953ad
Feril Agi <66341611+ferilagi@users.noreply.github.com>
2025-11-16 23:20:37 +0700

    update env key value using "="

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker build \
-    --build-arg APKMIRROR="turbo.net.id" \
-    -t ferilagi/invis:nginx_php_node .
-
-
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     -t ferilagi/webserver:php8.4-node24 \
