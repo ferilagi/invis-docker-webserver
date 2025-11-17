e2a33c1830b5eb79281b1354721d1e04e3e0b9b9
Feril Agi <66341611+ferilagi@users.noreply.github.com>
2025-11-17 20:06:13 +0700

    fix trigger workflow from tag

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
-    -t ferilagi/webserver:php8.4.14_node25 \
+    -t ferilagi/webserver:php8.4.14_node25.1 \
     -t ferilagi/webserver:latest \
     --push .
